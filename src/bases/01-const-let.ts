e90a15c6a44ccbcba63d554aad9bae042ca55180
Daniuh <naranjobenavidesd@gmail.com>
2026-04-28 12:04:23 -0500

    Se guarda el proyecto

---
+++ src/bases/01-const-let.ts
@@ -0,0 +1,9 @@
+const firstName: String = 'Daniel';
+const lastName  = 'Naranjo';
+
+let diceNumber = 5;
+
+const constainsLetterN = lastName.includes('J');
+
+
+console.log({ constainsLetterN, firstName, diceNumber });
---
+++ src/bases/02-template-string.ts
@@ -0,0 +1,7 @@
+
+const firstName = 'Daniel';
+const lastName  = "Naranjo's";
+
+const fullName = `El nombre es: ${firstName} ${lastName}`;
+
+console.log({ lastName });
